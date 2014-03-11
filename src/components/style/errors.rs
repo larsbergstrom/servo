42af28ac4f348dc46a58c906522b63e2425196b4
Lars Bergstrom <lars@lars.com>
2014-03-10 20:11:41 -0500

    Rust upgrade progress

--- src/components/style/errors.rs
+++ src/components/style/errors.rs
@@ -10,12 +10,12 @@ pub struct ErrorLoggerIterator<I>(I);
 
 impl<T, I: Iterator<Result<T, SyntaxError>>> Iterator<T> for ErrorLoggerIterator<I> {
     fn next(&mut self) -> Option<T> {
-        for result in **self {
+/*        for result in *self {
             match result {
                 Ok(v) => return Some(v),
                 Err(error) => log_css_error(error.location, format!("{:?}", error.reason))
             }
-        }
+        }*/
         None
     }
 }
